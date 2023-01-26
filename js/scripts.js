8f118bcad146583681757da37752936ee5b3003e
Regina Phalangy <nschallenberg@gmx.net>
2023-01-26 10:21:02 +0100

    Rewrote crossOut function

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,41 @@ function newItem(){
         // let text = document.createTextNode(inputValue);
         // li.appendChild(text);
     
-        //    if (inputValue === '') {
-        //      alert("You must write something!");
-        //    } else {
-        //      let list = document.querySelector('#list');
-        //      list.appendChild(li);
-        //    }
+        // if (inputValue === '') {
+        //     alert("You must write something!");
+        // } else {
+        //     let list = document.querySelector('#list');
+        //     list.appendChild(li);
+        // }
 
     // jQuery
-       let li = $('<li></li>')
-       let inputValue = $('#input').val();
-       li.append(inputValue);
+        let li = $('<li></li>')
+        let inputValue = $('#input').val();
+        li.append(inputValue);
        
-       if (inputValue === '') {
+        if (inputValue === '') {
             alert("You must write something!");
         } else {
             $('#list').append(li);
         }
 
     
-     //2. Crossing out an item from the list of items:
-       function crossOut() {
-             li.classList.toggle("strike");
-         }
-    
-         li.addEventListener("dblclick",crossOut);
+    // 2. Crossing out an item from the list of items:
+    // JavaScript
+        // function crossOut() {
+        //      li.classList.toggle("strike");
+        // }
+        // li.addEventListener("dblclick",crossOut);
+
+    // jQuery
+        function crossOut() {
+            li.toggleClass('strike');
+        }
+
+        li.on('dblclick', function crossOut() {
+            li.toggleClass('strike');
+        });
+
     
      //3(i). Adding the delete button "X": 
        let crossOutButton = document.createElement("crossOutButton");
